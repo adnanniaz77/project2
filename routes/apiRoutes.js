2cda5611f760245f45041cfad0b716f8f0b3f9cc
Adnan Niaz <adnanniaz77@yahoo.com>
2020-01-14 22:56:55 -0800

    front side is almost done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/api', (req, res) => {
-    res.send('You requested an api service...');
-    console.log('You have got an api request');
+router.get("/api", (req, res) => {
+    res.send("You requested an api service...");
+    console.log("You have got an api request");
 });
+
+router.post();
 
 module.exports = router;
