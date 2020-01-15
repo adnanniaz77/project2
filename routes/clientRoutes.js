58ee2e5dd7f43135540c7731c6c0b276692d4e52
Adnan Niaz <adnanniaz77@yahoo.com>
2020-01-14 22:27:29 -0800

    the ip location search is function now

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'))
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+
+router.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/about.html"));
 });
 
 module.exports = router;
--- views/app.js
+++ views/app.js
@@ -6,32 +6,33 @@ $(document).ready(function() {
         $("#data_table").empty();
 
         // check if input is given and is valid
-        if ($("#ipAdd").value == "") {
+        if ($("#ipAdd").val() == "") {
             alert("Please enter a valid ip address");
         }
 
-        let ipAdd = $("#ipAdd").val();
+        const ipAdd = $("#ipAdd").val();
+        const apiKey = "test";
 
         $.ajax({
             async: true,
             crossDomain: true,
-            url: `https://ejaz1-ip-geolocation-v1.p.rapidapi.com/ipgeo`,
+            url: `https://jkosgei-free-ip-geolocation-v1.p.rapidapi.com/${ipAdd}?api-key=${apiKey}`,
             method: "GET",
             headers: {
-                "x-rapidapi-host": "ejaz1-ip-geolocation-v1.p.rapidapi.com",
+                "x-rapidapi-host":
+                    "jkosgei-free-ip-geolocation-v1.p.rapidapi.com",
                 "x-rapidapi-key":
                     "99481dc939msh2e3c8e5dad0b58ep1fe7d4jsn424a1bf5d81f"
             }
         }).then(res => {
             console.log(res);
-            let imgSrc = res.country_flag;
+            let imgSrc = res.flag;
             $("#data_table").append(`
                     <tr><td></td><td><img src="${imgSrc}"></td></tr>
                     <tr><td>Country</td><td>${res.country_name}</td></tr>
-                    <tr><td>Country Code</td><td>${res.country_code2}</td></tr>
-                    <tr><td>Country Capital</td><td>${res.country_capital}</td></tr>
+                    <tr><td>Region</td><td>${res.region_code}</td></tr>
                     <tr><td>City</td><td>${res.city}</td></tr>
-                    <tr><td>zipcode</td><td>${res.zipcode}</td></tr>
+                    <tr><td>zipcode</td><td>${res.postal}</td></tr>
                     <tr><td>Latitude</td><td>${res.latitude}</td></tr>
                     <tr><td>Longitude</td><td>${res.longitude}</td></tr>
                 `);
