f88c8f65fa55dc87bd5a2af47f2d6313bce3ce64
Adnan Niaz <adnanniaz77@yahoo.com>
2020-01-26 15:57:06 -0800

    posts are getting from db and have to fix the id

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,18 +2,29 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
+// Get all Authors
 router.get("/all", (req, res) => {
     db.User.findAll({
         include: [db.Post]
     }).then(users => res.json(users));
 });
 
+// Register new Author
 router.post("/new", (req, res) => {
     db.User.create({
         username: req.body.username
     }).then(newUser => {
         res.json(newUser);
     });
+});
+
+// get author by ID
+router.get("/:id", (req, res) => {
+    db.User.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then(users => res.json(users));
 });
 
 module.exports = router;
